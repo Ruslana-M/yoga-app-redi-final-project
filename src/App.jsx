fee2526b5a9b48c2293cfcc1d89aa1768f3213ca
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-08 13:03:40 +0100

    add layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./layout/Nav";
+import Home from "./layout/Home";
+import Footer from "./layout/Footer";
+import Error from "./components/Error";
 
 function App() {
   return (
     <div className="App" data-theme="cupcake">
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nav" element={<Nav />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </main>
 
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className='flex flex-col items-center h-5/6 justify-center'>
+    <p className="text-6xl">Page not found</p>
+    <p className="text-3xl">It is ok to be lost sometimes.</p>
+    <p className="text-2xl underline decoration-solid decoration-sky-500"> Go back to Home</p>
+    
+    </div>
+  )
+}
+
+export default Error
