36f0eac62d1a25933e5f38fc7e12f40378611a7b
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-08 13:44:16 +0100

    add general style from DaisyUI

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Namaste</h1>
+        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <button className="btn bg-orange-400">Get started</button>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className="navbar justify-evenly bg-orange-200 text-primary-content">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">Calm Hub </a>
+    </div>
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1">
+        <li><a>Link</a></li>
+        <li>
+          <details>
+            <summary>
+              Login
+            </summary>
+            <ul className="p-2 bg-base-100 rounded-t-none">
+              <li><a>Register</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
   )
 }
 
