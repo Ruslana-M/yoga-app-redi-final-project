63a1b4b36020c4375b9771855701895a59aa2917
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-10 00:42:10 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+// import data from "../src/data/data-asana.json";
 import Nav from "./layout/Nav";
-import Home from "./layout/Home";
 import Footer from "./layout/Footer";
+
+import Home from "./layout/Home";
+import Login from "./components/Login";
 import Error from "./components/Error";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -12,8 +16,9 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/nav" element={<Nav />} />
-          <Route path="/footer" element={<Footer />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
           <Route path="/*" element={<Error />} />
         </Routes>
       </main>
