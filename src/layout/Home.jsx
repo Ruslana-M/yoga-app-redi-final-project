32563d3ded78a8081a004223c681d24a452d7285
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-10 23:14:34 +0100

    add cards to Home

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React from "react";
+import dataJson from "../data/data-asana.json";
 
 function Home() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-    <div className="hero-content text-center overflow-hidden">
-      <div className="max-w-md">
+    <div className="hero bg-base-200 min-h-screen flex flex-col">
+      <div className="hero-content text-center overflow-hidden w-full flex flex-col p-4">
         <h1 className="text-5xl font-bold">Namaste</h1>
-        <p className="py-6">Yoga practice improves flexibility, increases strength and reduces stress. Our app aimed to help you to create your own practice, using a large selection of yoga poses, selecting by category and difficulty.  Every pose features clear images and detailed instructions that you perform poses correctly.
-        Roll out your mat, make your selections and start your own created practice.
-</p>
+        <p className="py-6">
+          Yoga practice improves flexibility, increases strength and reduces
+          stress. Our app aimed to help you to create your own practice, using a
+          large selection of yoga poses, selecting by category and difficulty.
+          Every pose features clear images and detailed instructions that you
+          perform poses correctly. Roll out your mat, make your selections and
+          start your own created practice.
+        </p>
+      </div>
+
+      <div className="w-full flex gap-5 justify-center p-4 ">
+        {dataJson.asanas.map((i, index) => (
+          <div
+            className="card card-compact w-36 bg-base-100 shadow-xl "
+            key={index}
+          >
+            <figure>
+              <img src={i.url} alt="image of created cards with asanas" />
+            </figure>
+            <div className="card-body">
+              <h5 className="card-title">Description</h5>
+              <p>{i.difficulty}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-sm">See more</button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
