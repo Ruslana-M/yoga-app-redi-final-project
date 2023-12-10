bdf5b59ac06389e8ed1a783264f0b7b77a89897c
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-10 23:05:15 +0100

    add Register to Backendless

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Backendless from 'backendless';
 
 function Register() {
+    const [showError, setShowError] = useState(false)
+
+  function registerUser(e) {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+
+    if (password !== confirmPassword) {
+        setShowError(true)
+        
+    }else {
+        //creating Object for Backendless
+        const newUser = new Backendless.User()
+        newUser.email = email
+        newUser.password = password
+        newUser.username = username
+        newUser.userType = 'teacher'
+        //registing
+        Backendless.UserService.register(newUser)
+            .then((res) => { console.log(res)
+          }).catch(err => { console.log(` Check your password`,  err)
+          })
+    }
+  }
   return (
     <div className="w-screen h-screen flex justify-center items-center min-h-screen">
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form className="card-body" onSubmit={registerUser}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="username"
+              name="username"  
+              className="input input-bordered"
+              required
+            />
+          </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -12,6 +51,7 @@ function Register() {
             <input
               type="email"
               placeholder="email"
+              name="email"
               className="input input-bordered"
               required
             />
@@ -23,6 +63,7 @@ function Register() {
             <input
               type="password"
               placeholder="password"
+              name="password"
               className="input input-bordered"
               required
             />
@@ -33,7 +74,8 @@ function Register() {
             </label>
             <input
               type="password"
-              placeholder="password"
+              placeholder="confirm password"
+              name="confirmPassword"
               className="input input-bordered"
               required
             />
@@ -45,23 +87,21 @@ function Register() {
               </label>
               <div className="flex justify-between gap-6">
                 <label className="label cursor-pointer">
-                <input
+                  <input
                     type="radio"
                     name="radio-10"
                     className="radio radio-secondary"
                     checked
                   />
-                  <span className="label-text">Teacher</span>
-
+                  <span className="label-text  pl-2">Teacher</span>
                 </label>
                 <label className="label cursor-pointer">
-                <input
+                  <input
                     type="radio"
                     name="radio-10"
                     className="radio radio-secondary"
                   />
-                  <span className="label-text">Student</span>
-
+                  <span className="label-text pl-2">Student</span>
                 </label>
               </div>
             </div>
@@ -70,7 +110,7 @@ function Register() {
             <button className="btn btn-secondary">Register</button>
           </div>
         </form>
-      </div>
+      </div>   
     </div>
   );
 }
