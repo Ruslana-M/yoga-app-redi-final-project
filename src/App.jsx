28e177c67fee38f01b48ff6717cb3545cea4b24a
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-12 01:46:30 +0100

    added route for new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import data from "../src/data/data-asana.json";
 import Nav from "./layout/Nav";
 import Footer from "./layout/Footer";
-
 import Home from "./layout/Home";
 import Login from "./components/Login";
 import Error from "./components/Error";
 import Register from "./components/Register";
+import TeacherHome from "./components/TeacherHome";
+import CreateForm from "./components/CreateForm";
 import Backendless from 'backendless';
+import Preview from "./components/Preview";
 
 Backendless.serverURL = "https://eu-api.backendless.com"
 Backendless.initApp(process.env.REACT_APP_BACKENDLESS_ID, process.env.REACT_APP_BACKENDLESS_KEY);
@@ -22,6 +23,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/teacherHome" element={<TeacherHome />} />
+          <Route path="/createForm" element={<CreateForm />} />
+          <Route path="/preview" element={<Preview/>} />
+
+          
           <Route path="/*" element={<Error />} />
         </Routes>
       </main>
