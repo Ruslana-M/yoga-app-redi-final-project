2c19ffaf42604ddffcab37e81fd05fd6f65897db
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-12 01:48:29 +0100

    create a new component Preview

---
+++ src/components/Preview.jsx
@@ -0,0 +1,12 @@
+import Backendless from 'backendless'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+function Preview() {
+  return (
+    <div>Preview</div>
+  )
+}
+
+export default Preview
