3cf7bd97e98e6f8e5b1f85aa2d05c0f1e7887523
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-14 15:37:46 +0100

    created global state flowForPreview page and passed values to children

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,15 @@ import TeacherHome from "./components/TeacherHome";
 import CreateForm from "./components/CreateForm";
 import Backendless from 'backendless';
 import Preview from "./components/Preview";
+import { useState } from "react";
+
 
 Backendless.serverURL = "https://eu-api.backendless.com"
 Backendless.initApp(process.env.REACT_APP_BACKENDLESS_ID, process.env.REACT_APP_BACKENDLESS_KEY);
 
 function App() {
+  const [flowForPreview, setFlowForPreview] = useState();
+  
   return (
     <div className="App" data-theme="cupcake">
       <Nav />
@@ -23,9 +27,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/teacherHome" element={<TeacherHome />} />
-          <Route path="/createForm" element={<CreateForm />} />
-          <Route path="/preview" element={<Preview/>} />
+          <Route path="/teacherHome" element={<TeacherHome setFlowForPreview={setFlowForPreview}/>} />
+          <Route path="/preview" element={<Preview flowForPreview={flowForPreview} />} />
+          <Route path="/createForm" element={<CreateForm setFlowForPreview={setFlowForPreview} />} />
+
 
           
           <Route path="/*" element={<Error />} />
