dc671a8c5dded13c22ab2c92739909c752219efd
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-14 16:03:40 +0100

    received props, array of selected asanas ids; filtered out selected asanas id. in return get the data.

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,12 +1,37 @@
-import Backendless from 'backendless'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+
+import dataJson from "../data/data-asana.json";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+//  recieve here props
+// set const for array of selected asanas from database
+//  filtered out through objects the selected asanas ids : dataJson.asanas.filter((a) => asanaIds.includes(a.id))
+// in return checked if we get data from BE data
+
+function Preview({ flowForPreview }) {
+  const navigate = useNavigate();
 
 
-function Preview() {
+  // array of selected asanas ids  [4, 1, 3] in our data table
+  const asanaIds = flowForPreview.asanas;
+
+  // looping here to filter out selected asana's objects from datajson
+  const selectedAsanas = dataJson.asanas.filter((a) => asanaIds.includes(a.id));
+
+  // for (let i = 0; i < dataJson.asanas.length; i++) {
+  //   const element = dataJson.asanas[i];
+  //   if (element.id == flowForPreview.warmUps[0]) {
+  //     console.log(element);
+  //   }
+  // }
+
   return (
-    <div>Preview</div>
-  )
+    <div>
+      <p> {flowForPreview.title}</p>
+      <p> {flowForPreview.description} </p>
+      <p> {flowForPreview.level} </p>
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
