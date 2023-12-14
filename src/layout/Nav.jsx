17a5dc0eda6eddcb43943dcb71756e50fdf21f9e
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-14 15:46:00 +0100

    clean file

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 import CalmHubLogo from "../others/CalmHub_logo.png";
 
 function Nav() {
+ 
+
   return (
     <div className="navbar bg-orange-200 text-primary-content">
       <Link className="flex-1" to="/">
-        <img className='w-25' src={CalmHubLogo} alt="logo" />
+        <img className="w-25" src={CalmHubLogo} alt="logo" />
       </Link>
       <div className="flex-2">
         <ul className="menu menu-horizontal px-1">
@@ -15,17 +17,6 @@ function Nav() {
               Get Started
             </Link>
           </li>
-          {/* <li>
-          <details>
-            <summary>
-              Login
-            </summary>
-            <ul className="p-2 bg-base-100 rounded-t-none">
-              <li><a>Register</a></li>
-              <li><a>Logout</a></li>
-            </ul>
-          </details>
-        </li> */}
         </ul>
       </div>
     </div>
