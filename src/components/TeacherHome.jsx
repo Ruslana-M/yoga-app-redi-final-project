7672c4c9bb70744f05a6988f517b9dd468ef0370
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-18 12:33:15 +0100

    find yoga classes by user ID using backend. Add html layout, filter

--- src/components/TeacherHome.jsx
+++ src/components/TeacherHome.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import Backendless from "backendless";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function TeacherHome() {
+function TeacherHome({ user, setFlowForPreview }) {
   const navigate = useNavigate();
-  // const [yogaFlows, setYogaFlows] = useState([]); // add when i will have Backend
+  const [yogaFlows, setYogaFlows] = useState([]);
 
   function createClass() {
     navigate("/createForm");
   }
+  // to find yoga classes by user id
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    //  one user should see just his classes
+    // query builder creates a filter for Backendless.Data find
+    Backendless.Data.of("tamplate")
+      .find()
+      .then((res) => {
+        console.log(res);
+        setYogaFlows(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [user]);
+  function goToPreview(flow) {
+    setFlowForPreview(flow);
+    navigate("/preview");
+  }
+
   return (
     <div className="bg-base-200 min-h-screen flex flex-col">
       <div className="flex flex-center justify-between">
@@ -25,9 +48,28 @@ function TeacherHome() {
         {/* ** */}
       </div>
       <div className="hero-content text-center overflow-hidden w-full flex flex-col p-4 bg-slate-300">
-      <p>  Start to create your yoga class (+)</p>
-       {}
-
+        <div className="w-full flex flex-wrap gap-5 justify-center p-4 ">
+          {yogaFlows.map((i, index) => (
+            <div
+              className="card card-compact w-36 bg-base-100 shadow-xl "
+              key={index}
+            >
+               {/* <figure>
+                <img src={i.url} alt="image of yoga flow" />
+               </figure>  */}
+              <div className="card-body">
+                <p className="card-title"> <b> {i.title} </b> </p>
+                <p className="h-14 text-ellipsis overflow-hidden ">{i.description}</p>
+                <p>{i.difficulty}</p>
+                <div className="card-actions justify-end">
+                  <button onClick={() => goToPreview(i)} className="btn btn-sm">
+                    See more
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
